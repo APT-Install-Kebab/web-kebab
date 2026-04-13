d33665b24ef005b7c819e1a00dd60a4af212f6e7
elielicopter <eliroyer2410@gmail.com>
2026-04-13 07:59:35 +0000

    zbi

--- .github/workflows/create_container.sh
+++ .github/workflows/create_container.sh
@@ -6,4 +6,4 @@ if [ $old ];then
 	sudo docker kill $old
 fi
 sleep .5
-sudo docker run --detach --rm -p 80:80 --name kebab-web -t 5f9e7f39dee4
+sudo docker run --detach --rm -p 80:80 --name kebab-web -t 5f9e7f39dee4 .
